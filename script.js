50eda8dd02fa68dd6c1b2e5c6f04ec2c652cbdad
Magony Róbert Miklós <ctje64@INF.ELTE.HU>
2019-10-17 16:28:24 +0200

    Improved main menu

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
+
 gameState = 1;
 /*
     1 = Main menu
-    2 = Difficulty selection menu
-    3 = Ingame
-    4 = Post game menu
+    2 = Ingame
+    3 = Post game menu
 
     Start with main menu.
 */
+    x = canvas.getAttribute('width');
+    y = canvas.getAttribute('height');
+
 canvas.addEventListener('click', onClick);
 function drawMainMenu()
 {
-    x = canvas.getAttribute('width');
-    y = canvas.getAttribute('height');
+
     ctx.fillRect(x/8,y/12,6*x/8,2*y/12); //title
 
     ctx.fillRect(3*x/8, 5*y/12, 2*x/8, y/12); //play
-    ctx.fillRect(3*x/8, 8*y/12, 2*x/8, y/12); //about
+    ctx.fillRect(1.25*x/8, 8*y/12, 5.5*x/8, y/12); //difficulty
+
+    ctx.fillRect(1.25*x/8, 9.5*y/12, 1.5*x/8, y/12);
+    ctx.fillRect(3.25*x/8, 9.5*y/12, 1.5*x/8, y/12);
+    ctx.fillRect(5.25*x/8, 9.5*y/12, 1.5*x/8, y/12);
 }
 
 function onClick(e){
     switch (gameState){
         case(1):
         {
-            console.log(e.clientX);
+           //if (e.clientX - canvas.offsetLeft > )
+            console.log(e.clientX - canvas.offsetLeft);
+            console.log(e.clientY - canvas.offsetTop);
+        }
+        case(2):
+        {
+
+        }
+        case(3):
+        {
+
         }
     }
 }
